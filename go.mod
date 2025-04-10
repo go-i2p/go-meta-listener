635c0f8aca4c6c33553cca9bcadc6002e9c04444
eyedeekay <hankhill19580@gmail.com>
2025-04-10 18:39:54 -0400

    Create a multi-network mirror example

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/go-i2p/go-meta-listener
 
 go 1.23.5
+
+require (
+	github.com/go-i2p/onramp v0.33.92
+	github.com/opd-ai/wileedot v0.0.0-20241217172720-521d4175e624
+)
+
+require (
+	github.com/cretz/bine v0.2.0 // indirect
+	github.com/go-i2p/i2pkeys v0.33.10-0.20241113193422-e10de5e60708 // indirect
+	github.com/go-i2p/sam3 v0.33.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ mirror/listener.go
@@ -0,0 +1,107 @@
+package mirror
+
+import (
+	"log"
+	"net"
+
+	"github.com/go-i2p/go-meta-listener"
+	"github.com/go-i2p/onramp"
+
+	wileedot "github.com/opd-ai/wileedot"
+)
+
+type Mirror struct {
+	*meta.MetaListener
+	*onramp.Onion
+	*onramp.Garlic
+}
+
+var _ net.Listener = &Mirror{}
+
+func (m *Mirror) Close() error {
+	if err := m.MetaListener.Close(); err != nil {
+		log.Println("Error closing MetaListener:", err)
+	}
+	if err := m.Onion.Close(); err != nil {
+		log.Println("Error closing Onion:", err)
+	}
+	if err := m.Garlic.Close(); err != nil {
+		log.Println("Error closing Garlic:", err)
+	}
+	return nil
+}
+
+func NewMirror(name string) (*Mirror, error) {
+	inner := meta.NewMetaListener()
+	onion, err := onramp.NewOnion("metalistener-" + name)
+	if err != nil {
+		return nil, err
+	}
+	garlic, err := onramp.NewGarlic("metalistener-"+name, "127.0.0.1:7657", onramp.OPT_WIDE)
+	if err != nil {
+		return nil, err
+	}
+	ml := &Mirror{
+		MetaListener: inner,
+		Onion:        onion,
+		Garlic:       garlic,
+	}
+	return ml, nil
+}
+
+// Listen creates a new Mirror instance and sets up listeners for TLS, Onion, and Garlic.
+// It returns the Mirror instance and any error encountered during setup.
+// name is the domain name used for the TLS listener, required for Let's Encrypt.
+// addr is the email address used for Let's Encrypt registration.
+// It is recommended to use a valid email address for production use.
+func Listen(name string, addr string, certdir string, hiddenTls bool) (net.Listener, error) {
+	ml, err := NewMirror(name)
+	if err != nil {
+		return nil, err
+	}
+	cfg := wileedot.Config{
+		Domain:         name,
+		AllowedDomains: []string{name},
+		CertDir:        certdir,
+		Email:          addr,
+	}
+	tlsListener, err := wileedot.New(cfg)
+	if err != nil {
+		return nil, err
+	}
+	if err := ml.AddListener("tls", tlsListener); err != nil {
+		return nil, err
+	}
+	if hiddenTls {
+		onionListener, err := ml.Onion.ListenTLS()
+		if err != nil {
+			return nil, err
+		}
+		if err := ml.AddListener("onion", onionListener); err != nil {
+			return nil, err
+		}
+		garlicListener, err := ml.Garlic.ListenTLS()
+		if err != nil {
+			return nil, err
+		}
+		if err := ml.AddListener("garlic", garlicListener); err != nil {
+			return nil, err
+		}
+	} else {
+		onionListener, err := ml.Onion.Listen()
+		if err != nil {
+			return nil, err
+		}
+		if err := ml.AddListener("onion", onionListener); err != nil {
+			return nil, err
+		}
+		garlicListener, err := ml.Garlic.Listen()
+		if err != nil {
+			return nil, err
+		}
+		if err := ml.AddListener("garlic", garlicListener); err != nil {
+			return nil, err
+		}
+	}
+	return ml, nil
+}
