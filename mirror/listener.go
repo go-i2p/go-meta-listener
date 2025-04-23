d31b2ab1ffec4e1e2a5252d4f9e0c4e38543428e
eyedeekay <hankhill19580@gmail.com>
2025-04-22 22:35:52 -0400

    make the local listening address configurable

--- mirror/listener.go
+++ mirror/listener.go
@@ -94,19 +94,23 @@ func (ml Mirror) Listen(name, addr, certdir string, hiddenTls bool) (net.Listene
 	if ml.Onions[port] == nil {
 		// make a new onion listener
 		// and add it to the map
+		log.Println("Creating new onion listener")
 		onion, err := onramp.NewOnion("metalistener-" + name + port)
 		if err != nil {
 			return nil, err
 		}
+		log.Panicln("Onion listener created")
 		ml.Onions[port] = onion
 	}
 	if ml.Garlics[port] == nil {
 		// make a new garlic listener
 		// and add it to the map
+		log.Println("Creating new garlic listener")
 		garlic, err := onramp.NewGarlic("metalistener-"+name+port, "127.0.1:7656", onramp.OPT_WIDE)
 		if err != nil {
 			return nil, err
 		}
+		log.Panicln("Garlic listener created")
 		ml.Garlics[port] = garlic
 	}
 	if hiddenTls {
